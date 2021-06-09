806ee9223f4755faabbb62dcf56243a7079fa7a7
amina1000 <amina1000@yandex.ru>
2021-06-09 18:35:32 +0300

    Урок 7. Задача1. Подумайте о функционале вашего приложения заметок. Какие экраны там могут быть, помимо основного со списком заметок? Как можно использовать меню и всплывающее меню в вашем приложении? Не обязательно сразу пытаться реализовать весь этот функционал,  достаточно создать макеты и структуру, а реализацию пока заменить на заглушки или всплывающие уведомления (Toast). Используйте подход Single Activity для отображения экранов. В качестве примера: на главном экране приложения у вас список всех заметок, при нажатии на заметку открывается экран с этой заметкой. В меню главного экрана у вас есть иконка поиска по заметкам и сортировка. В меню «Заметки» у вас есть иконки «Переслать» (или «Поделиться»), «Добавить ссылку или фотографию к заметке».

---
+++ app/src/main/java/com/example/notes/Settings.java
@@ -0,0 +1,15 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+
+/**
+ * homework com.example.notes
+ *
+ * @author Amina
+ * 08.06.2021
+ */
+public class Settings {
+    public static final String SHARED_PREFERENCE_NAME = "FragmentNavigation";
+    public static final String FONT_SIZE = "FONT_SIZE";
+    public static int fontSize = 20;
+}
---
+++ app/src/main/java/com/example/notes/SettingsFragment.java
@@ -0,0 +1,56 @@
+package com.example.notes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class SettingsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+       initView(view);
+    }
+
+    private void initView(View view) {
+        fontSizeSetting(view);
+    }
+
+    private void fontSizeSetting(View view) {
+        TextView fontSize = view.findViewById(R.id.font_size);
+        fontSize.setText(String.format(Locale.getDefault(),"%d",Settings.fontSize));
+        Button btnChangeFont = view.findViewById(R.id.btn_font);
+        btnChangeFont.setOnClickListener(v -> {
+            Settings.fontSize = Integer.parseInt(fontSize.getText().toString());
+            writeSettings();
+        });
+
+    }
+
+    private void writeSettings() {
+        // Специальный класс для хранения настроек
+        SharedPreferences sharedPref = requireActivity().getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+        // Настройки сохраняются посредством специального класса editor
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(Settings.FONT_SIZE, Settings.fontSize);
+        editor.apply();
+
+    }
+}
