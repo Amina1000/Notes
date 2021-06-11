bf9f7af471cf62879c1d363421cff548ad3d45d4
amina1000 <amina1000@yandex.ru>
2021-06-11 19:28:51 +0300

    Урок 8. Задача 1. Создайте список ваших заметок.

---
+++ app/src/main/java/com/example/notes/NoteAdapter.java
@@ -0,0 +1,71 @@
+package com.example.notes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * homework com.example.notes
+ *
+ * @author Amina
+ * 11.06.2021
+ */
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
+    private final List<Note> dataSource;
+
+    // Передаём в конструктор источник данных
+    // В нашем случае это массив, но может быть и запрос к БД
+    public NoteAdapter(List<Note> dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    // Создать новый элемент пользовательского интерфейса
+    // Запускается менеджером
+    @NonNull
+    @Override
+    public NoteAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        // Создаём новый элемент пользовательского интерфейса
+        // Через Inflater
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item, viewGroup, false);
+        // Здесь можно установить всякие параметры
+        return new ViewHolder(v);
+    }
+
+    // Заменить данные в пользовательском интерфейсе
+    // Вызывается менеджером
+    @Override
+    public void onBindViewHolder(@NonNull NoteAdapter.ViewHolder viewHolder, int i) {
+        // Получить элемент из источника данных (БД, интернет...)
+        // Вынести на экран, используя ViewHolder
+        viewHolder.getTextView().setText(dataSource.get(i).getName());
+    }
+
+    // Вернуть размер данных, вызывается менеджером
+    @Override
+    public int getItemCount() {
+        return dataSource.size();
+    }
+
+    // Этот класс хранит связь между данными и элементами View
+    // Сложные данные могут потребовать несколько View на один пункт списка
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+}
--- app/src/main/java/com/example/notes/NoteList.java
+++ app/src/main/java/com/example/notes/NoteList.java
@@ -1,26 +1,28 @@
 package com.example.notes;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class NoteList extends Fragment {
 
-    private LinearLayout linearLayout;
+    private RecyclerView recyclerView;
     private Context context;
+    private final List<Note> noteLinkedList = new LinkedList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,10 +34,10 @@ public class NoteList extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        linearLayout = view.findViewById(R.id.list_container);
+        recyclerView = view.findViewById(R.id.recycler_view_notes);
         context = getContext();
         initPopupMenu(view);
-        addNotesToList();
+        initRecyclerView();
     }
 
     private void initPopupMenu(View view) {
@@ -65,22 +67,28 @@ public class NoteList extends Fragment {
         }
     }
 
-    private void addNotesToList() {
+    private void initRecyclerView() {
 
+        createNoteFromArrays();
+        // Эта установка служит для повышения производительности системы
+        recyclerView.setHasFixedSize(true);
+
+        // Будем работать со встроенным менеджером
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Установим адаптер
+        NoteAdapter adapter = new NoteAdapter(noteLinkedList);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    private void createNoteFromArrays() {
         String[] notesArray = getResources().getStringArray(R.array.notes);
         for (int i = 0, notesArrayLength = notesArray.length; i < notesArrayLength; i++) {
             String note = notesArray[i];
-            TextView notesView = new TextView(context);
-            notesView.setText(note);
-            notesView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.textSize));
-            notesView.setTypeface(null, Typeface.BOLD_ITALIC);
-            final int fi = i;
-            notesView.setOnClickListener(v -> {
-                v.setBackgroundResource(R.color.teal_700);
-                ((NoteScreen) requireActivity()).openNoteScreen(new Note(note,
-                        getResources().getStringArray(R.array.descriptions)[fi]));
-            });
-            linearLayout.addView(notesView);
+            noteLinkedList.add(new Note(note,getResources().getStringArray(R.array.descriptions)[i]));
         }
     }
 }
