f31390baf853c0ac1e8d4104287f347c25c99496
amina1000 <amina1000@yandex.ru>
2021-06-23 01:58:43 +0300

    поправила adapter, настроила Firebase.

--- app/src/main/java/com/example/notes/NoteSource.java
+++ app/src/main/java/com/example/notes/NoteSource.java
@@ -9,7 +9,8 @@ package com.example.notes;
 public interface NoteSource {
     Note getNoteData(int position);
     int size();
-
+    boolean isGroupItem(int position);
+    String getGroupTitle(int position);
     void deleteNoteData(int position);
     void updateNoteData(int position, Note noteData);
     void addNoteData(Note noteData);
